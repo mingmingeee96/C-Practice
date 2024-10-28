2ec2cbeaf165d5e38c25478e5d541aaedd80ad8b
DESKTOP-9BFGVDK\com <mjk6984@gmail.com>
2024-10-28 17:48:36 +0900

    241028 office

---
+++ Data structure/Chapter06/Q1/CLinkedList.c
@@ -0,0 +1,108 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "CLinkedList.h"
+
+// 리스트의 초기화
+void ListInit(List * plist)
+{
+	plist->tail = NULL;
+    plist->cur = NULL;
+    plist->before = NULL;
+    plist->numOfData = 0;
+}
+
+// 노드의 삽입
+// 머리에 삽입
+void LInsertFront(List * plist, Data data)
+{
+	Node * newNode = (Node *)malloc(sizeof(Node));
+    newNode->data = data;
+    
+    if(plist->tail == NULL)
+    {
+    	plist->tail = newNode;
+        newNode->next = newNode;
+    }
+   	else
+    {
+    	newNode->next = plist->tail->next;
+        plist->tail->next = newNode;
+    }
+    
+    (plist->numOfData)++;
+}
+
+// 꼬리에 삽입
+void LInsert(List * plist, Data data)
+{
+	Node * newNode = (Node *)malloc(sizeof(Node));
+    newNode->data = data;
+    
+    if(plist->tail == NULL)
+    {
+    	plist->tail = newNode;
+        newNode->next = newNode;
+    }
+   	else
+    {
+    	newNode->next = plist->tail->next;
+        plist->tail->next = newNode;
+        plist->tail = newNode;
+    }
+    
+    (plist->numOfData)++;
+}
+
+// 데이터 조회
+// 첫 번째 노드
+int LFirst(List * plist, Data * pdata)
+{
+	if(plist->tail == NULL)
+    	return FALSE;
+    
+    plist->before = plist->tail;
+    plist->cur = plist->tail->next;
+    
+    *pdata = plist->cur->data;
+    return TRUE;
+}
+
+// 두 번째 이후 노드
+int LNext(List * plist, Data * pdata)
+{
+	if(plist->tail == NULL)
+    	return FALSE;
+    
+    plist->before = plist->cur;
+    plist->cur = plist->cur->next;
+    
+    *pdata = plist->cur->data;
+    return TRUE;
+}
+
+// 노드의 삭제
+Data LRemove(List * plist)
+{
+	Node * rpos = plist->cur;
+    Data rdata = rpos->data;
+    
+    if(rpos == plist->tail)
+    {
+    	if(plist->tail == plist->tail->next)
+        	plist->tail == NULL;
+        else
+        	plist->tail = plist->before;
+    }
+    
+    plist->before->next = plist->cur->next;
+    plist->cur = plist->before;
+    
+    free(rpos);
+    (plist->numOfData)--;
+    return rdata;
+}
+
+int LCount(List * plist)
+{
+    return plist->numOfData;
+}
