97d4016c8f80061c276390bb3ce24147299d79b9
DESKTOP-9BFGVDK\com <mjk6984@gmail.com>
2024-12-17 14:33:13 +0900

    241217 ch.14 part2 done

---
+++ Data structure/Chapter14/ALGraph/DLinkedList.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "DLinkedList.h"
+
+// 리스트 초기화
+void ListInit(List * plist)
+{
+	plist->head = (Node*)malloc(sizeof(Node));
+	plist->head->next = NULL;
+	plist->comp = NULL;
+	plist->numOfData = 0;
+}
+
+
+// 노드 삽입
+void FInsert(List * plist, LData data)
+{
+	Node * newNode = (Node*)malloc(sizeof(Node));
+	newNode->data = data;
+
+	newNode->next = plist->head->next;
+	plist->head->next = newNode;
+
+	(plist->numOfData)++;
+}
+
+void SInsert(List * plist, LData data)
+{
+	Node * newNode = (Node*)malloc(sizeof(Node));
+	Node * pred = plist->head;
+	newNode->data = data;
+
+	while(pred->next != NULL &&
+		plist->comp(data, pred->next->data) != 0)
+	{
+		pred = pred->next;
+	}
+
+	newNode->next = pred->next;
+	pred->next = newNode;
+
+	(plist->numOfData)++;
+}
+
+
+void LInsert(List * plist, LData data)
+{
+	if(plist->comp == NULL)
+		FInsert(plist, data);
+	else
+		SInsert(plist, data);
+}
+
+
+// 데이터 조회
+int LFirst(List * plist, LData * pdata)
+{
+	if(plist->head->next == NULL)
+		return FALSE;
+
+	plist->before = plist->head;
+	plist->cur = plist->head->next;
+
+	*pdata = plist->cur->data;
+	return TRUE;
+}
+
+int LNext(List * plist, LData * pdata)
+{
+	if(plist->cur->next == NULL)
+		return FALSE;
+
+	plist->before = plist->cur;
+	plist->cur = plist->cur->next;
+
+	*pdata = plist->cur->data;
+	return TRUE;
+}
+
+// 노드 삭제
+LData LRemove(List * plist)
+{
+	Node * rpos = plist->cur;
+	LData rdata = rpos->data;
+
+	plist->before->next = plist->cur->next;
+	plist->cur = plist->before;
+
+	free(rpos);
+	(plist->numOfData)--;
+	return rdata;
+}
+
+
+// 데이터 전체 수 조회
+int LCount(List * plist)
+{
+	return plist->numOfData;
+}
+
+// 정렬 기준 등록
+void SetSortRule(List * plist, int (*comp)(LData d1, LData d2))
+{
+	plist->comp = comp;
+}
